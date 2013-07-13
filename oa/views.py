622ba5bfbabb9bb25d820b1647f462523b97db21
Jeff SMith <jeff@allthingsdork.com>
2013-07-13 14:33:52 -0500

    Began rework with basic nodes and started Twitter bootstrap integration

--- oa/views.py
+++ oa/views.py
@@ -5,7 +5,10 @@ from django.shortcuts import render_to_response
 
 
 def index(request):
-    
-    return render_to_response('index.html',context_instance=RequestContext(request))
+    return render_to_response('index.html',
+                              context_instance=RequestContext(request))
 
+
+def add_node(request):
+    pass
 
